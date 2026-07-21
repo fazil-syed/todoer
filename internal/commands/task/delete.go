b98cc73be9a9eb59c7b199e9b562722fb7a34573
Syed Fazil <syedfazil539@gmail.com>
2026-07-21 23:47:04 +0530

    feat(tasks): multi ids support for crud command

--- internal/commands/task/delete.go
+++ internal/commands/task/delete.go
@@ -6,34 +6,49 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/fazil-syed/todoer/internal/models"
 	"github.com/urfave/cli/v3"
 )
 
 func (c *TaskCommand) deleteTasksHandler(ctx context.Context, cmd *cli.Command) error {
-	taskID := cmd.Args().First()
+	args := cmd.Args().Slice()
 
-	if taskID == "" {
+	if len(args) == 0 {
 		fmt.Println("missing task id")
 		return nil
 	}
-	id, err := strconv.Atoi(taskID)
-	if err != nil {
-		return err
-	}
-	task, err := c.tasksRepository.GetById(ctx, int64(id))
 
-	if err != nil {
-		return err
+	ids := make([]int64, 0, len(args))
+
+	for _, arg := range args {
+		id, err := strconv.Atoi(arg)
+		if err != nil {
+			return fmt.Errorf("invalid task id %q: %w", arg, err)
+		}
+		ids = append(ids, int64(id))
 	}
-	if err := c.tasksRepository.Delete(ctx, int64(id)); err != nil {
-		return err
+	var tasks []models.Task
+	for _, id := range ids {
+		task, err := c.tasksRepository.GetById(ctx, int64(id))
+
+		if err != nil {
+			return err
+		}
+		if err := c.tasksRepository.Delete(ctx, int64(id)); err != nil {
+			return err
+		}
+		tasks = append(tasks, *task)
+
 	}
 
-	fmt.Println("Deleted task")
+	fmt.Println("Deleted tasks")
 	printer := NewTaskPrinter(os.Stdout)
 	defer printer.Flush()
 	printer.PrintSingleTaskHeadLine()
-	printer.PrintSingleTask(*task, false)
+	for _, task := range tasks {
+		printer.PrintSingleTask(task, false)
+
+	}
 	return nil
 }
 
